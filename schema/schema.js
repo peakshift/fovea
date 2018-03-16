9a39416f7d88a03a2975f8a6a7d2c5dfe7acf0da
Early Charlemagne <early@peakshift.com>
2018-03-16 01:31:55 +0100

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,73 @@
+const invoiceSchema = {
+  id: '/invoice',
+  type: 'object',
+  properties: {
+    clientId: {
+      type: 'string'
+    },
+    dateWritten: {
+      type: 'string',
+      format: 'date-time'
+    },
+    referenceNumber: {
+      type: 'string'
+    },
+    invoiceNumber: {
+      type: 'number'
+    },
+    details: {
+      type: 'array',
+      minItems: 1,
+      items: {
+        type: 'object',
+        properties: {
+          label: {
+            type: 'string',
+            enum: [ 'SERVICE', 'PRODUCT' ]
+          },
+          description: {
+            type: 'object',
+            properties: {
+              title: {
+                type: 'string'
+              },
+              text: {
+                type: 'string'
+              },
+              list: {
+                type: "array",
+                items: {
+                  "type": "string"
+                }
+              },
+            }
+          },
+          quantity: {
+            type: 'number'
+          },
+          total: {
+            type: 'number'
+          }
+        },
+        required: [
+          "label",
+          "description",
+          "quantity",
+          "total"
+        ]
+      }
+    }
+  },
+  required: [
+    'clientId',
+    'dateWritten',
+    'referenceNumber',
+    'invoiceNumber',
+    'details'
+  ],
+  additionalProperties: false
+}
+
+module.exports = {
+  invoiceSchema
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const serverlessWebpack = require('serverless-webpack')
+
+module.exports = {
+  entry: serverlessWebpack.lib.entries,
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+}
