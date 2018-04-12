30b3c11bb0ba444decc0e22cbc36de0fdc659091
Early Charlemagne <early@peakshift.com>
2018-04-12 19:24:59 +0200

    feat(measures): create function to calculate various performance ratios

---
+++ lib/transformer/measures.py
@@ -0,0 +1,55 @@
+def gpr(gross_profit, net_sales):
+    """Calculate the Gross Profit Ratio.
+
+    Args:
+        gross_profit (int): The gross profit.
+        net_sales (int): The net sales.
+
+    Returns:
+        float: Returns a float if successul.
+        bool: False if not successful.
+
+    """
+    return False
+
+def opcr(op_cost, net_sales):
+    """Calculate the Operating Cost Ratio.
+
+    Args:
+        op_cost (int): The operating cost.
+        net_sales (int): The net sales.
+
+    Returns:
+        float: Returns a float if successul.
+        bool: False if not successful.
+
+    """
+    return False
+
+def oppr(op_profit, net_sales):
+    """Calculate the Operating Profit Ratio, also called the Net Profit Ratio.
+
+    Args:
+        op_profit (int): The operating profit.
+        net_sales (int): The net sales.
+
+    Returns:
+        float: Returns a float if successul.
+        bool: False if not successful.
+
+    """
+    return False
+
+def opr(net_profit, tl_assets):
+    """Calculate the Overal Profitability Ratio.
+
+    Args:
+        net_profit (int): The net profit.
+        tl_assets (int): The total assets.
+
+    Returns:
+        float: Returns a float if successul.
+        bool: False if not successful.
+
+    """
+    return False
