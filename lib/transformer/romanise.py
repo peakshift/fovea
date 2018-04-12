66ab343ce2c854a7a51f1577bf31cfad9cffe5cc
Early Charlemagne <early@peakshift.com>
2018-04-12 19:20:43 +0200

    feat(conversion): create function to convert cyrillic letters in a sentence to latin letters (#1)

--- lib/transformer/romanise.py
+++ lib/transformer/romanise.py
@@ -1,56 +1,82 @@
 # -*- coding: utf-8 -*-
 
-# Translitteration of a bulgarian letter to a latin letter.
-#
-# We transform the strings that gets passed to unicode, as 
-# capital cylyric letters do not get lowered if 
-# the string.lower() method is used, unless we convertd the  
-# strings to unicode data types first. 
-#
-# @param letter {string} - a cylyric letter to look up to transform it to a corresponding latin one. 
-# @return - the corresponding latin letter
-
 def romanise(letter):
+	"""Translitteration of a bulgarian letter to a latin letter.
+
+	We transform the strings that gets passed to unicode, as
+	capital cylyric letters do not get lowered if
+	the string.lower() method is used, unless we convertd the
+	strings to unicode data types first.
+
+    Args:
+        text (str): A cylyric letter to look up to transform it to a corresponding latin one.
+
+    Returns:
+        str: The corresponding latin letter if successul, otherwise the initial character
+
+    """
+
 	bulgarian_alphabet = {
-		u"а": "a", 
-		u"б": "b", 
-		u"в": "v", 
-		u"г": "g", 
-		u"д": "d", 
-		u"е": "e", 
-		u"ж": "zh", 
-		u"з": "z", 
-		u"и": "i", 
-		u"й": "y", 
-		u"к": "k", 
-		u"л": "l", 
-		u"м": "m", 
-		u"н": "n", 
-		u"о": "o", 
-		u"п": "p", 
-		u"р": "r", 
-		u"с": "s", 
-		u"т": "t", 
-		u"y": "u", 
-		u"ф": "f", 
-		u"х": "kh", 
-		u"ц": "ts", 
-		u"ч": "ch", 
-		u"ш": "sh", 
-		u"щ": "sht", 
-		u"ъ": "u", 
-		u"ь": "'", 
-		u"ю": "yu", 
+		u"а": "a",
+		u"б": "b",
+		u"в": "v",
+		u"г": "g",
+		u"д": "d",
+		u"е": "e",
+		u"ж": "zh",
+		u"з": "z",
+		u"и": "i",
+		u"й": "y",
+		u"к": "k",
+		u"л": "l",
+		u"м": "m",
+		u"н": "n",
+		u"о": "o",
+		u"п": "p",
+		u"р": "r",
+		u"с": "s",
+		u"т": "t",
+		u"y": "u",
+		u"ф": "f",
+		u"х": "kh",
+		u"ц": "ts",
+		u"ч": "ch",
+		u"ш": "sh",
+		u"щ": "sht",
+		u"ъ": "u",
+		u"ь": "'",
+		u"ю": "yu",
 		u"я": "ya",
 	}
 
-	unicode_letter = letter.decode('utf-8')
-	lowercase_letter = unicode_letter.lower()
-	print(bulgarian_alphabet[lowercase_letter])
- 
-# Example usage
+	lowercase_letter = letter.lower()
 
-romanise("Ю") # expects yu
-romanise("ю") # expects yu
-romanise("ж") # expects zh
-romanise("х") # expects kh
+	return bulgarian_alphabet[lowercase_letter] if lowercase_letter in bulgarian_alphabet else letter
+
+def cyr_to_latin(text):
+	"""Transform sentences that are in cyrillic into sentences using latin letters
+
+	The words in the sentence are broken down into letters,
+	each letter is converted to latin using the function romanise,
+	and the letters are reformed into a whole sentence
+
+    Args:
+        text (str): The sentence to be converted
+
+    Returns:
+        str: Converted string is returned.
+
+    """
+
+	word_list = text.split()
+
+	for index, word in enumerate(word_list):
+		word_translation = "".join(romanise(x) for x in word)
+
+		word_list[index] = word_translation
+
+	return " ".join(str(text) for text in word_list)
+
+
+# sentence = 'ТАКСА ЗА ВЪТРЕШНОБАНКОВ ПРЕВОДЕЛ'
+# print(cyr_to_latin(sentence)) # taksa za vutreshnobankov prevodel
