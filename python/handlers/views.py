62c13cd8632998bf73e7b2439d7df9531fafb74c
micey969 <micey_969@hotmail.com>
2018-07-14 11:39:46 -0400

    docs(files): correct the structure of the files

---
+++ python/handlers/views.py
@@ -0,0 +1,6 @@
+import common.validate as validate
+from flask import jsonify, request, Flask
+
+app = Flask(__name__)
+
+
---
+++ python/run.py
@@ -0,0 +1,7 @@
+from handlers.views import app
+import flask
+
+app.run(debug=True,
+        threaded=True,
+        host='0.0.0.0',
+        port=5000)
