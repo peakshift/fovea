59850d2d31cf73112c6d3d7f0b01160fa3c854c2
micey969 <micey_969@hotmail.com>
2018-07-18 14:19:11 -0400

    feat(database): add handler for database manipulation

--- python/handlers/views.py
+++ python/handlers/views.py
@@ -1,4 +1,3 @@
-import json
 import common.validate as validation
 from flask import jsonify, request, Flask
 
@@ -11,7 +10,6 @@ def post():
     request_data = request.get_json()
 
     message = validate.isvalid(request_data)
-    print(message)
     if message is True:
         return jsonify({"msg": "Project created."}), 201
     else:
